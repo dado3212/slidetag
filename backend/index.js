e84ace91f37ba1408444f8e58cbf7a2aa26d06e9
Alex Beals <alexcbeals@gmail.com>
2018-04-10 19:41:57 -0400

    QUITE a few print statements and uncaught errors, but functional ability to get a number of posts for a given hashtag that aren't videos and are above a certain number of likes

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ const app = express();
 const { Builder, By, Key, until, Condition } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const { sleep } = require('sleep');
+const request = require('request');
+const rp = require('request-promise');
+// const zlib = require('zlib');
+// const https = require('https');
 
 function parseImage(imageHTML) {
   if (imageHTML.includes("img alt=")) {
     match = imageHTML.match(/<a href="(.*?)">.*<img alt="([\s\S]*?)".*src="(.*?)"/);
-    console.log(imageHTML);
     return {
       url: `https://www.instagram.com${match[1]}`,
       caption: `${match[2]}`,
@@ -23,7 +26,30 @@ function parseImage(imageHTML) {
   }
 }
 
-async function pollHashtag(hashtag, number) {
+async function fleshOut(item) {
+  console.log(item.url);
+  let body = await rp(item.url);
+  let rawInfo = body.match(/window\._sharedData = ({.*);<\/script>/);
+  if (rawInfo.length >= 2) {
+    let data = JSON.parse(rawInfo[1]);
+    let modified = {
+      timestamp: (data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["taken_at_timestamp"]),
+      caption: (data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_media_to_caption"]["edges"][0]["node"]["text"]),
+      user: (data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["owner"]["username"]),
+      comment_count: (data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_media_to_comment"]["count"]),
+      like_count: (data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_media_preview_like"]["count"]),
+      is_video: !!(data["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["is_video"]),
+      url: item.url,
+      image_url: item.image_url,
+    };
+    return modified;
+  } else {
+    console.log(item.url);
+    return null;
+  }
+}
+
+async function pollHashtag(hashtag, number, min_likes) {
   // Build a new driver for the browser searching
   let driver = await new Builder()
     .forBrowser('chrome')
@@ -35,6 +61,7 @@ async function pollHashtag(hashtag, number) {
     await driver.get(`https://www.instagram.com/explore/tags/${hashtag}/`);
     // Create an empty array
     let pictures = [];
+    let fleshedOutPictures = [];
     // _6d3hm _mnav9
 
     // Inject code to scroll up and down until there's enough photos
@@ -48,9 +75,13 @@ async function pollHashtag(hashtag, number) {
       let imageHTMLs = await Promise.all(imageTags.map(a => a.getAttribute('innerHTML')));
       let imageElements = imageHTMLs.map(element => parseImage(element));
 
-      imageElements.forEach((item) => {
+      imageElements.forEach(async (item) => {
         if (!pictures.includes(item)) {
           pictures.push(item);
+          let a = await fleshOut(item);
+          if (a && a.like_count >= min_likes && !a.is_video) {
+            fleshedOutPictures.push(a);
+          }
         }
       })
 
@@ -61,19 +92,19 @@ async function pollHashtag(hashtag, number) {
 
       // Wait one second to load
       await sleep(1);
-    } while (pictures.length < number);
+    } while (fleshedOutPictures.length < number);
 
-    pictures = pictures.slice(0, number);
+    fleshedOutPictures = fleshedOutPictures.slice(0, number);
 
-    console.log(pictures);
-    console.log(pictures.length);
+    console.log(fleshedOutPictures);
+    console.log(fleshedOutPictures.length);
   } finally {
     await driver.quit();
   }
 }
 
-// Get the most recent 100 photos tagged 'dog'
-pollHashtag('dog', 150);
+// Get the most recent 100 photos tagged 'dog' with over 20 likes
+pollHashtag('dog', 150, 20);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
